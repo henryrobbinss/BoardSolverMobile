3499133766237e405d2b1f904426d033568c7e09
Henry Robbins <hdr29@comcast.net>
2024-06-04 14:42:24 -0400

    Initial Commit

---
+++ Board Solver/Board_SolverApp.swift
@@ -0,0 +1,17 @@
+//
+//  Board_SolverApp.swift
+//  Board Solver
+//
+//  Created by Henry Robbins on 6/4/24.
+//
+
+import SwiftUI
+
+@main
+struct Board_SolverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Board SolverUITests/Board_SolverUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Board_SolverUITestsLaunchTests.swift
+//  Board SolverUITests
+//
+//  Created by Henry Robbins on 6/4/24.
+//
+
+import XCTest
+
+final class Board_SolverUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
