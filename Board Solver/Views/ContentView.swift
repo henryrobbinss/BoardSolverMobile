00763c83705a4cb8e970f65f95c1111823db13f2
Henry Robbins <hdr29@comcast.net>
2024-06-21 14:21:49 -0400

    New Camera View

--- Board Solver/Views/ContentView.swift
+++ Board Solver/Views/ContentView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 import CoreML
 import Vision
+import RealityKit
 
 struct ContentView: View
 {
@@ -18,33 +19,70 @@ struct ContentView: View
     {
         ZStack
         {
-            CameraView(image: $viewModel.currentFrame,  isLocked: $viewModel.isLocked)
-                .scaledToFill()
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
-            
-            HStack{
-                Button 
-                {
-                    print("scanning")
-                    if let image = $viewModel.currentFrame.wrappedValue
+            ARViewContainer().edgesIgnoringSafeArea(.all)
+            VStack{
+                Spacer()
+                
+                BoardView()
+                    .padding(.bottom, 20)
+                
+                
+                HStack{
+                    Button
                     {
-                        let uiImage = UIImage.init(cgImage: image)
-                        classifier.detect(uiImage: uiImage)
+                        print("scanning")
+                        if let image = $viewModel.currentFrame.wrappedValue
+                        {
+                            let uiImage = UIImage.init(cgImage: image)
+                            classifier.detect(uiImage: uiImage)
+                        }
+                    } label: {
+                        Label("", image: "scan_prompt")
                     }
-                } label: {
-                    Label("", image: "scan_prompt")
+                    .frame(maxWidth: 175)
+                    Button {
+                        print("locking")
+                        viewModel.toggleLock()
+                    } label: {
+                        Label("", image: "lock_prompt")
+                    }
+                    .frame(maxWidth: 175)
                 }
-                .frame(maxWidth: 175)
-                Button {
-                    print("locking")
-                    viewModel.toggleLock()
-                } label: {
-                    Label("", image: "lock_prompt")
-                }
-                .frame(maxWidth: 175)
+                .padding()
             }
-            .padding()
+            .padding(.bottom, 50)
         }
-        .padding()
+        .ignoresSafeArea(.all)
     }
+}
+
+struct ARViewContainer: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> ARView {
+        
+        let arView = ARView(frame: .zero)
+
+        // Create a cube model
+        let mesh = MeshResource.generateBox(size: 0.1, cornerRadius: 0.005)
+        let material = SimpleMaterial(color: .gray, roughness: 0.15, isMetallic: true)
+        let model = ModelEntity(mesh: mesh, materials: [material])
+        model.transform.translation.y = 0.05
+
+        // Create horizontal plane anchor for the content
+        let anchor = AnchorEntity(.plane(.horizontal, classification: .any, minimumBounds: SIMD2<Float>(0.2, 0.2)))
+        anchor.children.append(model)
+
+        // Add the horizontal plane anchor to the scene
+        arView.scene.anchors.append(anchor)
+
+        return arView
+        
+    }
+    
+    func updateUIView(_ uiView: ARView, context: Context) {}
+    
+}
+
+#Preview {
+    BoardView()
 }
