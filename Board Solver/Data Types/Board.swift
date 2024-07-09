f260808184477ec39b2ba56ead34920d52d22bb2
Henry Robbins <hdr29@comcast.net>
2024-07-09 13:57:47 -0400

    fix major bug

--- Board Solver/Data Types/Board.swift
+++ Board Solver/Data Types/Board.swift
@@ -85,13 +85,13 @@ class Board
         let bwidth = rightBoard - leftBoard
         let bheight = topBoard - bottomBoard
         let columns = [(bwidth/14)+leftBoard, (bwidth/14 + bwidth/7)+leftBoard,
-                       (width/14 + 2*width/7)+leftBoard, (width/14 + 3*width/7)+leftBoard,
-                       (width/14 + 4*width/7)+leftBoard, (width/14 + 5*width/7)+leftBoard,
-                       (width/14 + 6*width/7)+leftBoard]
+                       (bwidth/14 + 2*bwidth/7)+leftBoard, (bwidth/14 + 3*bwidth/7)+leftBoard,
+                       (bwidth/14 + 4*bwidth/7)+leftBoard, (bwidth/14 + 5*bwidth/7)+leftBoard,
+                       (bwidth/14 + 6*bwidth/7)+leftBoard]
         
-        let rows = [(bwidth/12)+bottomBoard, (bwidth/12 + bheight/6)+bottomBoard,
-                    (bwidth/12 + 2*bheight/6)+bottomBoard, (bwidth/12 + 3*bheight/6)+bottomBoard,
-                    (bwidth/12 + 4*bheight/6)+bottomBoard, (bwidth/12 + 5*bheight/6)+bottomBoard]
+        let rows = [(bheight/12)+bottomBoard, (bheight/12 + bheight/6)+bottomBoard,
+                    (bheight/12 + 2*bheight/6)+bottomBoard, (bheight/12 + 3*bheight/6)+bottomBoard,
+                    (bheight/12 + 4*bheight/6)+bottomBoard, (bheight/12 + 5*bheight/6)+bottomBoard]
         
         for p in pieces{
             let rowsLength = rows.count
@@ -115,4 +115,71 @@ class Board
         }
         return board
     }
+    
+//    static func getSolverString(board: [[Int]]) -> String {
+//        /*
+//             for row in board:
+//                 for i in range(len(row)):
+//                     if row[i] == 0:
+//                         continue
+//                     elif next == "red" and row[i] == RED_PIECE:
+//                         finalString = "".join([finalString, str(i + 1)])
+//                         if len(pending_moves) != 0:
+//                             finalString = "".join([finalString, str(pending_moves[0])])
+//                             pending_moves[0] = pending_moves[-1]
+//                             pending_moves.pop()
+//                         else:
+//                             next = "yellow"
+//                     elif next == "yellow" and row[i] == YELLOW_PIECE:
+//                         finalString = "".join([finalString, str(i + 1)])
+//                         if len(pending_moves) != 0:
+//                             finalString = "".join([finalString, str(pending_moves[0])])
+//                             pending_moves[0] = pending_moves[-1]
+//                             pending_moves.pop()
+//                         else:
+//                             next = "red"
+//                     else:
+//                         pending_moves.append(str(i + 1))
+//             finalString = "".join([finalString] + pending_moves)
+//             return finalString
+//         */
+//        var flippedbBoard = board
+//        flippedbBoard.reverse()
+//        var pending: [Int] = []
+//        var result = ""
+//        // TEMP UNTIL WE ADD MENU
+//        var first = "r"
+//        var next = first
+//        // 0 - red, 1 - yellow, 2 - nothing
+//        var RED = 0, YELLOW = 1
+//        for row in board {
+//            for i in Sequence(row.count) {
+//                if row[i] == 2 {
+//                    continue
+//                } else if next == "red" && piece == RED {
+//                    result = result +
+//                        finalString = "".join([finalString, str(i + 1)])
+//                        if len(pending_moves) != 0:
+//                            finalString = "".join([finalString, str(pending_moves[0])])
+//                            pending_moves[0] = pending_moves[-1]
+//                            pending_moves.pop()
+//                        else:
+//                            next = "yellow"
+//                    elif next == "yellow" and row[i] == YELLOW_PIECE:
+//                        finalString = "".join([finalString, str(i + 1)])
+//                        if len(pending_moves) != 0:
+//                            finalString = "".join([finalString, str(pending_moves[0])])
+//                            pending_moves[0] = pending_moves[-1]
+//                            pending_moves.pop()
+//                        else:
+//                            next = "red"
+//                    else:
+//                        pending_moves.append(str(i + 1))
+//                }
+//            }
+//        }
+//        
+//        //finalString = "".join([finalString] + pending_moves)
+//        return result
+//    }
 }
