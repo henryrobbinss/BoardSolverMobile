13d347e8ee455acb9752fa3b33a3096551e5ab88
Yanzhen Lu <jimlo7760@gmail.com>
2024-10-04 23:02:59 -0400

    Add detailed comments to menuView

--- Board Solver/Views/menuView.swift
+++ Board Solver/Views/menuView.swift
@@ -4,10 +4,12 @@
 //
 //  Created by Alex Mattoni on 7/12/24.
 //
+//  This view represents the main menu of the Board Solver app, providing navigation to different game modes and infomation.
 
 import SwiftUI
 
-struct MenuView: View 
+// The main menu view for the Board Solver app.
+struct MenuView: View
 {
     var body: some View
     {
@@ -16,11 +18,12 @@ struct MenuView: View
         {
             ZStack
             {
-                //Background
+                // Backgound: set to white, covering the entire screen.
                 Color.white.ignoresSafeArea(.all)
                 
                 VStack
                 {
+                    // Display the title text.
                     VStack {
                         Text("BOARD")
                             .font(.custom("KoHo-Medium", size: 90))
@@ -34,19 +37,21 @@ struct MenuView: View
                             .monospacedDigit()
                     }
                     
+                    // Display the main image below the title.
                     Image("image1")
                         .resizable()
                         .scaledToFit()
                         .frame(width: 300, height: 230)
                         .offset(y: -20)
                     
+                    // Display a prompt to select a game.
                     Text("Select a Game Below")
                         .font(.custom("KoHo-Medium", size: 30))
                         .foregroundColor(Color.black)
                         .multilineTextAlignment(.center)
                         .monospacedDigit()
                     
-                    // Four In A Row
+                    // Navigation link to the "Four In A Row" game menu.
                     NavigationLink
                     {
                         FourInARowMenuView().navigationBarTitle("").navigationBarHidden(true)
@@ -55,7 +60,7 @@ struct MenuView: View
                         Label("", image: "fourinarow_prompt")
                     }
                     
-                    // Word scramble
+                    // Navigation link to the "Word Scramble" game menu.
                     NavigationLink
                     {
                         WordScrambleMenuView().navigationBarTitle("").navigationBarHidden(true)
@@ -64,7 +69,7 @@ struct MenuView: View
                         Label("", image: "scribble_prompt")
                     }
                     
-                    // About
+                    // Navigation link to the "About" view.
                     NavigationLink
                     {
                         AboutView().navigationBarTitle("").navigationBarHidden(true)
