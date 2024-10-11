70dacadad16441fb23880ae18ca139390421ca96
Yanzhen Lu <jimlo7760@gmail.com>
2024-10-11 16:22:26 -0400

    Update View in MenuView to the latest swift specification
    NavigationView has been deprecated , and should be replaced by NavigationStack with new parameters. Detail can be found on https://developer.apple.com/documentation/swiftui/navigationviewstyle

--- Board Solver/Views/menuView.swift
+++ Board Solver/Views/menuView.swift
@@ -13,78 +13,60 @@
 import SwiftUI
 
 // The main menu view for the Board Solver app.
-struct MenuView: View
-{
-    var body: some View
-    {
-        // Selection buttons
-        NavigationView
-        {
-            ZStack
-            {
-                // Backgound: set to white, covering the entire screen.
-                Color.white.ignoresSafeArea(.all)
-                
-                VStack
-                {
+struct MenuView: View {
+    var body: some View {
+        NavigationStack {
+            ZStack {
+                // Background: set to white, covering the entire screen.
+                Color.white
+                    .ignoresSafeArea()
+
+                VStack {
                     // Display the title text.
                     VStack {
                         Text("BOARD")
                             .font(.custom("KoHo-Medium", size: 90))
-                            .foregroundColor(Color.black)
+                            .foregroundColor(.black)
                             .multilineTextAlignment(.center)
-                            .monospacedDigit()
                         Text("SOLVER")
                             .font(.custom("KoHo-Medium", size: 90))
-                            .foregroundColor(Color.black)
+                            .foregroundColor(.black)
                             .multilineTextAlignment(.center)
-                            .monospacedDigit()
                     }
-                    
+
                     // Display the main image below the title.
                     Image("image1")
                         .resizable()
                         .scaledToFit()
                         .frame(width: 300, height: 230)
                         .offset(y: -20)
-                    
+
                     // Display a prompt to select a game.
                     Text("Select a Game Below")
                         .font(.custom("KoHo-Medium", size: 30))
-                        .foregroundColor(Color.black)
+                        .foregroundColor(.black)
                         .multilineTextAlignment(.center)
-                        .monospacedDigit()
-                    
+
                     // Navigation link to the "Four In A Row" game menu.
-                    NavigationLink
-                    {
-                        FourInARowMenuView().navigationBarTitle("").navigationBarHidden(true)
-                    } label:
-                    {
-                        Label("", image: "fourinarow_prompt")
+                    NavigationLink(destination: FourInARowMenuView()
+                        .toolbar(.hidden, for: .navigationBar)) {
+                        Image("fourinarow_prompt")
                     }
-                    
+
                     // Navigation link to the "Word Scramble" game menu.
-                    NavigationLink
-                    {
-                        WordScrambleMenuView().navigationBarTitle("").navigationBarHidden(true)
-                    } label:
-                    {
-                        Label("", image: "scribble_prompt")
+                    NavigationLink(destination: WordScrambleMenuView()
+                        .toolbar(.hidden, for: .navigationBar)) {
+                        Image("scribble_prompt")
                     }
-                    
+
                     // Navigation link to the "About" view.
-                    NavigationLink
-                    {
-                        AboutView().navigationBarTitle("").navigationBarHidden(true)
-                    } label:
-                    {
-                        Label("", image: "about_prompt")
+                    NavigationLink(destination: AboutView()
+                        .toolbar(.hidden, for: .navigationBar)) {
+                        Image("about_prompt")
                     }
-                    //.offset(y: -90)
                 }
             }
-        }.navigationViewStyle(StackNavigationViewStyle())
+        }
     }
 }
 
